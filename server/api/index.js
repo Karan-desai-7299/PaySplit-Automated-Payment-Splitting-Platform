2dcec2a0bef8bc6d1ec0724fe7c62ea37558270f
Karansinh Desai <karansinhdesai91@gmail.com>
2026-09-25 17:15:19 +0530

    fix(vercel): direct export of express app in api/index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,11 +1,3 @@
 import app from '../index.js';
-import { connectDB } from '../db.js';
 
-export default async function handler(req, res) {
-  try {
-    await connectDB();
-  } catch (err) {
-    console.error('Serverless DB connect error:', err);
-  }
-  return app(req, res);
-}
+export default app;
